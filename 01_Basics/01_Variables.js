0c14cb1e65fe7814cd96c6acc476d17d1f9c5b5c
Sundas-Nawaz <sundasnawaz101@gmail.com>
2024-07-09 16:32:05 +0000

    variables and datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 12344
+let accountEmail = 'sundas@gmail.com'
+var accountPassword = "12344"
+accountCity= "lahore"
+
+
+// accountId = 33444 //not allowed because we can't change const values
+accountEmail = 'ahmad@gmail.com'
+accountPassword = "6556"
+accountCity = "karachi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all js code as a newer version
+//alert ("hi"); // we are using node js not browser so its not working
+let number = null;
+
+console.log(typeof number);
+console.log(typeof null);
+console.log(typeof undefined);
