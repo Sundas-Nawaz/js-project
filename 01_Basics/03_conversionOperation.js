a3a20386aad6c879016d0418924e3fd0e7b67384
Sundas-Nawaz <sundasnawaz101@gmail.com>
2024-07-10 09:04:05 +0000

    Conversion operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let scoreNum = Number(score);
+
+// console.log(typeof scoreNum);
+// console.log(scoreNum);
+
+let isLoggedIn = "101";
+
+console.log(isLoggedIn);
+let booleanIsLogIn = Boolean(isLoggedIn)
+console.log(booleanIsLogIn);
+console.log(typeof(booleanIsLogIn));
